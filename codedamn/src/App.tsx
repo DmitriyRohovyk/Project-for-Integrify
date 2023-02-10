6f27ba16337797dcd8e0c49190fffd0e37789005
DmitriyRohovyk <108825303+DmitriyRohovyk@users.noreply.github.com>
2023-02-10 20:03:26 +0200

    first commit

---
+++ codedamn/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { MainRoutes } from './Routes/MainRoutes';
+import { Header } from './components/Header';
+function App() {
+  return (
+   <div>
+     <Header />
+     <MainRoutes />
+   </div>
+  );
+}
+
+export default App;
+
